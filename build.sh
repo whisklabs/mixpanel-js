9739b77d7b73c044f261f24cb779a461a99b36a8
David Brooks <david@whisk.co.uk>
2015-01-30 16:41:39 +0000

    Moved build to remove CDN version and wrap in Whisk namespace

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-java -jar $1 --js mixpanel.js --js_output_file mixpanel.min.js --compilation_level ADVANCED_OPTIMIZATIONS --output_wrapper "(function() {
+java -jar $1 --js mixpanel.js --js_output_file mixpanel.min.js --compilation_level ADVANCED_OPTIMIZATIONS --output_wrapper "(function(namespace) {
 %output%
-})();"
+})(window.WhiskNamespace.libs);"
 
-java -jar $1 --js mixpanel-jslib-snippet.js --js_output_file mixpanel-jslib-snippet.min.js --compilation_level ADVANCED_OPTIMIZATIONS --define='LIB_URL="//cdn.mxpnl.com/libs/mixpanel-2.2.js"'
+java -jar $1 --js mixpanel-jslib-snippet.js --js_output_file mixpanel-jslib-snippet.min.js --compilation_level ADVANCED_OPTIMIZATIONS --output_wrapper "(function(namespace) {
+%output%
+})(window.WhiskNamespace.libs);"
 
